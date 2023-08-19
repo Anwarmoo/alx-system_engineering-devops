fabf57e5a495f4d99f76dd6b392fc74698d6354f
MOHAMED ADAN <anwarmoh389@gmail.com>
2023-08-19 21:51:42 +0000

    Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,34 +1,15 @@
-exec { 'update':
-  command => '/usr/bin/apt-get -y update',
+# custom http header response NGiNX
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-
-package { 'nginx':
-  ensure  => installed,
-  require => Exec['update']
-}
-
-file_line { 'redirect':
-  ensure   => 'present',
-  path     => '/etc/nginx/sites-available/default',
-  after    => 'server_name _;',
-  line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  require => Package['nginx'],
-}
-
-file_line { 'header':
+-> package {'nginx':
   ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _;',
-  line   => 'add_header X-Served-By "$HOSTNAME";',
-  require => Package['nginx'],
 }
-
-file { '/var/www/html/index.html':
-  content => 'Holberton School',
-  require => Package['nginx'],
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+-> exec {'run2':
+  command => '/usr/sbin/service nginx start',
 }
